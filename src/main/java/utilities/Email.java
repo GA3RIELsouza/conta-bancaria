9592204b05bf766ef5db7f04987fbc15b56d6114
Gabriel Leandro de Souza <gabrielleandrodesouza11@gmail.com>
2024-10-10 18:58:44 -0300

    Refatoração de código

--- src/main/java/utilities/Email.java
+++ src/main/java/utilities/Email.java
@@ -5,15 +5,16 @@ import java.util.regex.Matcher;
 
 public class Email
 {
-	static final String regexEmail = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+	public static final String regexEmail = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
 	 
-	static public boolean isEmail(String email)
+	public static boolean isEmail(String email)
 	{
 		Pattern pattern = Pattern.compile(regexEmail);
         Matcher matcher = pattern.matcher(email);
 
         if(matcher.matches())
             return true;
-        return false;
+        else
+        	return false;
 	}
 }

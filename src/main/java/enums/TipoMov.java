bb294a13ece0aa21da77805148a7c25a9578f591
Gabriel Leandro de Souza <gabrielleandrodesouza11@gmail.com>
2024-11-19 20:26:11 -0300

    Enum TipoMov ajustado

--- src/main/java/enums/TipoMov.java
+++ src/main/java/enums/TipoMov.java
@@ -2,7 +2,19 @@ package enums;
 
 public enum TipoMov
 {
-    aumentaSaldo,
-    diminuiSaldo,
-    naoAlteraSaldo;
+    aumentaSaldo(0),
+    diminuiSaldo(1),
+    naoAlteraSaldo(2);
+
+    private final int id;
+
+    TipoMov(int id)
+    {
+        this.id = id;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
 }
